fd3aee6cd93f4ba51e0cf4cb58e0d26f3041641f
Anshul Chauhan <anshulchauhan1995@gmail.com>
2019-08-21 10:54:53 +0530

    Features# added pg gem

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem 'sassc-rails', '>= 2.1.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- db/migrate/20190730062837_add_column_to_user.rb
+++ db/migrate/20190730062837_add_column_to_user.rb
@@ -2,11 +2,9 @@ class AddColumnToUser < ActiveRecord::Migration[5.2]
  
   def change
   	 add_column :users, :name, :string
-  	 add_column :users, :mobile, :string ,null: false,default: ""
+  	 add_column :users, :mobile, :string ,null: false,default: "",  unique: true
   	 add_column :users, :gender, :string
   	 add_column :users, :dob, :date
-  	 add_column :users, :username, :string, null: false, default: ""
+  	 add_column :users, :username, :string, null: false, default: "",  unique: true
   end
-  add_index :users, :mobile,                unique: true
-  add_index :users, :username,                unique: true
 end

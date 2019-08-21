cc248b13035960d2e741c46674cc07dd8ffb6ba9
Anshul Chauhan <anshulchauhan1995@gmail.com>
2019-08-21 12:18:37 +0530

    Features commited

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable,:omniauthable, omniauth_providers: [:google_oauth2, :facebook ]
+         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: [:google_oauth2, :facebook ]
 
 
  attr_writer :login
